a0bc041c33631529d6e0692cec7a71c5d5932eab
kundankrishna8084 <81030641+kundankrishna8084@users.noreply.github.com>
2025-01-23 14:50:48 +0530

    Second Commit

--- src/main/java/com/jsp/hibernate_many_to_one_unidirectional/controller/InsertEmployeeCompanyController.java
+++ src/main/java/com/jsp/hibernate_many_to_one_unidirectional/controller/InsertEmployeeCompanyController.java
@@ -1,9 +1,12 @@
 package com.jsp.hibernate_many_to_one_unidirectional.controller;
 
+import com.jsp.hibernate_many_to_one_unidirectional.dao.EmployeeCompanyDao;
+
 public class InsertEmployeeCompanyController {
 	public static void main(String[] args) {
 		
 		
+	EmployeeCompanyDao dao = new EmployeeCompanyDao();
 	}
 
 }
